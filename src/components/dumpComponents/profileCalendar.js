7cc3b6a69bb74d38dc2a2c478fd3d95a23b6f31d
stefan <stefanats@hotmail.com>
2018-03-30 13:40:21 +0200

    server_prod

--- src/components/dumpComponents/profileCalendar.js
+++ src/components/dumpComponents/profileCalendar.js
@@ -23,9 +23,27 @@ let style4 = {
   width: '50px',
   background: 'blue',
   position: 'absolute',
+  transform: 'translate(0, -50%)',
+  top: '50%',
+  left: '0',
+}
+let style5 = {
+  height: '50px',
+  width: '50px',
+  background: 'blue',
+  position: 'absolute',
   transform: 'translate(-50%, -50%)',
   top: '50%',
   left: '50%',
+}
+let style6 = {
+  height: '50px',
+  width: '50px',
+  background: 'blue',
+  position: 'absolute',
+  transform: 'translate(-100%, -50%)',
+  top: '50%',
+  left: '100%',
 }
 
 class ProfileWorkClock extends React.Component{
@@ -38,19 +56,31 @@ class ProfileWorkClock extends React.Component{
       id: 0,
     }
   }
+  slide = e => {
+    alert(e)
+  }
   render(){
+    let left = this.state.arrLeft;
+    let middle = this.state.arrMiddle;
+    let right = this.state.arrRight;
     return(
       <div className={css.calendarCircle}>
         <div className={css.calendarCircleWrapper}>
-          <div style={style1}></div>
+          <div style={style1} onClick={() => this.slide('left')}></div>
           <div style={style2}>
             <div style={style4}>
-              
-            </div>    
-      
-
+              <div>
+                3
+              </div>
+            </div>
+            <div style={style5}>
+             middle
+            </div>
+            <div style={style6}>
+             right
+            </div>
           </div>
-          <div style={style3}></div>
+          <div style={style3} onClick={() => this.slide('right')}></div>
         </div>
       </div>
     )
